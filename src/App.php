9fe574273943abd77a34cf6ea68b8e53eaf1009b
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2022-08-14 20:29:18 +0200

    send message

--- src/App.php
+++ src/App.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use DateTime;
+use Exception;
 use PierreMiniggio\DatabaseFetcher\DatabaseFetcher;
 
 class App
@@ -35,6 +35,54 @@ class App
             return;
         }
       
-      
+        $username = substr($path, 1);
+
+        $bot = $this->bot;
+        $chatId = $this->channelId;
+
+        $sendMessageCurl = curl_init();
+        curl_setopt_array($sendMessageCurl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://api.telegram.org/bot' . $bot . '/sendMessage?chat_id=' . $chatId . '&text=' . $message
+        ]);
+        $sendMessageCurlResponse = curl_exec($sendMessageCurl);
+        $httpCode = curl_getinfo($sendMessageCurl)['http_code'];
+        curl_close($sendMessageCurl);
+
+        if ($httpCode !== 200) {
+            throw new Exception('Send message request failed with code ' . $httpCode . ' : ' . $sendMessageCurlResponse);
+        }
+
+        if ($sendMessageCurlResponse === false) {
+            throw new Exception('No body for send message request');
+        }
+
+        $sendMessageCurlJsonResponse = json_decode($sendMessageCurlResponse, true);
+
+        if (! $sendMessageCurlJsonResponse) {
+            throw new Exception('Bad JSON for send message request : ' . $sendMessageCurlResponse);
+        }
+
+        if (empty($sendMessageCurlJsonResponse['ok'])) {
+            throw new Exception('Send message request not ok : ' . $sendMessageCurlResponse);
+        }
+
+        if (! isset($sendMessageCurlJsonResponse['result'])) {
+            throw new Exception('Send message request missing result key : ' . $sendMessageCurlResponse);
+        }
+
+        $fetchedMessage = $sendMessageCurlJsonResponse['result'];
+
+        if (! isset($fetchedMessage['message_id'])) {
+            throw new Exception('Send message request missing result->message_id key : ' . $sendMessageCurlResponse);
+        }
+
+        $messageId = $fetchedMessage['message_id'];
+
+        if (! $messageId) {
+            throw new Exception('Send message request has empty result->message_id value : ' . $sendMessageCurlResponse);
+        }
+
+        var_dump($messageId)
     }
 }
